404406586217fb6f2fe6616f3db7f2e581daf8c3
lveez <65928629+lveez@users.noreply.github.com>
2023-05-21 20:33:00 +0100

    Modified the batch file.

--- BO_StratTester.bat
+++ BO_StratTester.bat
@@ -1,2 +1,16 @@
+@echo off
+if exist bin (
+	goto launch )
+cd ..
+if exist bin (
+	goto launch )
+cd ..
+if exist bin (
+	goto launch )
+echo Could not find launcher_ldr.exe
+pause
+exit
+
+:launch
 cd bin
 start launcher_ldr.exe game_mod.dll ../BlackOps.exe +set fs_game "mods/Strat Tester"
